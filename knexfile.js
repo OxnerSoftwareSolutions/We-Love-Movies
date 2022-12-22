13da1e86d27d44d14d840cd0605e0fcf9e182b93
Benji <boxner5107@gmail.com>
2022-12-22 14:34:12 -0500

    updated Knex file, fixed spelling error

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,13 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  PRODUCTION_DATABASE_URL = "postgresql://postgres@localhost/postgres",DEVELOPMENT_DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: DEVELOPMENT_DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -20,7 +20,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: PRODUCTION_DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
